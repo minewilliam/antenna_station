be696b7e4f79168627c99e07769af4c1a50f9241
William Longpre <williamlongpre@hotmail.com>
2019-12-10 20:07:16 -0500

    Added NavSatFix messages (unstable)

--- src/antenna_controller.cpp
+++ src/antenna_controller.cpp
@@ -1,10 +1,14 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
+#include <string.h>
+#include "sensor_msgs/NavSatFix.h"
 #include <sstream>
 
+double Longitude, Latitude, Altitude;
+bool AutoMode = true;
+
 void MoveAntennaCallback(const std_msgs::String::ConstPtr& msg);
-void AntennaPositionCallback(const std_msgs::String::ConstPtr& msg);
+void AntennaPositionCallback(const sensor_msgs::NavSatFix& msg);
 void RoverPositionCallback(const std_msgs::String::ConstPtr& msg);
 
 int main(int argc, char **argv)
@@ -12,10 +16,10 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "AutoControl",0);
     ros::NodeHandle n;
     ros::Subscriber sub1 = n.subscribe("MoveAntenna", 1000, MoveAntennaCallback);
-    ros::Subscriber sub2 = n.subscribe("StationGPS", 1000, AntennaPositionCallback);
+    ros::Subscriber sub2 = n.subscribe("nmea_topic_driver", 1000, AntennaPositionCallback);
     ros::Subscriber sub3 = n.subscribe("RoverPos", 1000, RoverPositionCallback);
     ros::Rate loop_rate(10);
-
+    
     while(ros::ok())
     {
         ros::spinOnce();
@@ -29,9 +33,11 @@ void MoveAntennaCallback(const std_msgs::String::ConstPtr& msg)
     ROS_INFO("Target angle: [%s]", msg->data.c_str());
 }
 
-void AntennaPositionCallback(const std_msgs::String::ConstPtr& msg)
+void AntennaPositionCallback(const sensor_msgs::NavSatFix& msg)
 {
-    ROS_INFO("Station Position: [%s]", msg->data.c_str());
+    Longitude = msg.longitude;
+    Latitude = msg.latitude;
+    Altitude = msg.altitude;
 }
 
 void RoverPositionCallback(const std_msgs::String::ConstPtr& msg)
